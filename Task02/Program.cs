19324780e78dd9cd391004db757f9d3a50c67019
Oleg Shnyrev <o-shnyrov@mail.ru>
2023-01-14 23:35:13 +0300

    Добавлены решения задач

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+Console.WriteLine($"max = {number1}"); 
+else if (number1 < number2)
+Console.WriteLine($"max = {number2}"); 
+else Console.WriteLine("Нет решения");
---
+++ Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int size = 3;
+int count = 1;
+int max = number1;
+while (count < size)
+{count = count + 1;
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+    if (max < number2)
+    {
+    max = number2;
+    };
+};
+Console.WriteLine(max); 
